fee2e7f9cf21704d111a2383e09b80cd8622a3e2
Ferru Lopez <lopezdelcidferrydejesus@gmail.com>
2025-09-09 09:24:06 -0600

    menu

---
+++ main.py
@@ -0,0 +1,13 @@
+
+while True:
+    print("1- Calcular el factorial de un numero")
+    print("2- SUma de los primeros N numeros naturales")
+    print("3- Calcular el n-ésimo número de Fibonacci")
+    print("4- Contar cuántas veces aparece una letra en una palabra")
+    print("5- Invertir una cadena de texto")
+    print("6- Calcular la potencia de un número (base^exponente)")
+    print("7- Salir")
+    opcion = input("Ingrese su opcion")
+    match opcion:
+        case "1":
+
